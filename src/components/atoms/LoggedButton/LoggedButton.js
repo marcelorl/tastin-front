ca48c67d1a87f73dce3b06df80ce89ae23e104e0
marcelorl <marcelo_rl2@hotmail.com>
2017-04-17 23:36:27 -0300

    LoggedButton button style

--- src/components/atoms/LoggedButton/LoggedButton.js
+++ src/components/atoms/LoggedButton/LoggedButton.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 const LoggedButton = (props) => (
   <IconMenu
     iconButtonElement={
-      <IconButton><MoreVertIcon /></IconButton>
+      <FloatingActionButton mini secondary={true}>
+        <MoreVertIcon />
+      </FloatingActionButton>
     }
     targetOrigin={{horizontal: 'right', vertical: 'top'}}
     anchorOrigin={{horizontal: 'right', vertical: 'top'}}
