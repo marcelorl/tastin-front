3840f0b28318b703fdd8aad4e9a6086400e77d47
marcelorl <marcelo_rl2@hotmail.com>
2017-04-19 18:06:00 -0300

    semistandard implementation

--- src/components/atoms/LoginButton/LoginButton.js
+++ src/components/atoms/LoginButton/LoginButton.js
@@ -6,7 +6,7 @@ import './LoginButton.css';
 const LoginButton = (props) => {
   const { auth } = props;
 
-  return <RaisedButton className="login-button" label="Login" secondary onClick={auth.login.bind(this)}/>;
+  return <RaisedButton className='login-button' label='Login' secondary onClick={auth.login.bind(this)} />;
 };
 
 LoginButton.muiName = 'FlatButton';
