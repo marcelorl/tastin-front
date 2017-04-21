7b54f4678ed59cce18bdbed5844b1cf8a1f0175b
marcelorl <marcelo_rl2@hotmail.com>
2017-04-21 17:10:21 -0300

    Authentication improvements

--- src/components/atoms/LoggedButton/LoggedButton.js
+++ src/components/atoms/LoggedButton/LoggedButton.js
@@ -5,25 +5,33 @@ import MenuItem from 'material-ui/MenuItem';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import Divider from 'material-ui/Divider';
+import get from 'lodash/get';
 
-const LoggedButton = (props) => (
-  <IconMenu
-    iconButtonElement={
-      <FloatingActionButton mini secondary={true}>
-        <MoreVertIcon />
-      </FloatingActionButton>
-    }
-    targetOrigin={{horizontal: 'right', vertical: 'top'}}
-    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-  >
-    <MenuItem primaryText={props.auth.getProfile().name} disabled />
-    <Divider />
-    <MenuItem primaryText="Sign out" onClick={props.auth.logout} />
-  </IconMenu>
-);
+const LoggedButton = (props) => {
+  const userName = get(props.auth.getProfile(), 'name', 'John Doe');
+
+  return (
+    <IconMenu
+      iconButtonElement={
+        <FloatingActionButton mini secondary={true}>
+          <MoreVertIcon />
+        </FloatingActionButton>
+      }
+      targetOrigin={{horizontal: 'right', vertical: 'top'}}
+      anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+    >
+      <MenuItem primaryText={userName} disabled />
+      <Divider />
+      <MenuItem primaryText="Sign out" onClick={props.auth.logout} />
+    </IconMenu>
+  );
+}
 
 LoggedButton.propTypes = {
-  auth: PropTypes.shape({})
+  auth: PropTypes.shape({
+    getProfile: PropTypes.func,
+    logout: PropTypes.func
+  })
 };
 
 export default LoggedButton;
--- src/components/pages/App/App.Container.js
+++ src/components/pages/App/App.Container.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import App from './App';
 import AuthService from '../../../utils/AuthService';
 
 const auth = new AuthService(process.env.REACT_APP_AUTH0_CLIENT_ID, process.env.REACT_APP_AUTH0_DOMAIN);
 
-class AppContainer extends Component {
-  render() {
-    return <App auth={auth} />
-  }
-}
+const AppContainer = () =>
+  <App auth={auth} />;
 
 export default AppContainer;
