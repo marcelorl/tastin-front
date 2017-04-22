f1a14276fa63ae56ed7e8fedc453bbe18f0b7d9b
Marcelo RL <marcelo.lima@sprinklr.com>
2017-04-22 06:03:25 -0300

    react-devtools and reducers implemented

--- src/components/pages/App/App.Container.js
+++ src/components/pages/App/App.Container.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
 import App from './App';
 import AuthService from '../../../utils/AuthService';
+import axios from 'axios';
 
-const auth = new AuthService(process.env.REACT_APP_AUTH0_CLIENT_ID, process.env.REACT_APP_AUTH0_DOMAIN);
+const authService = new AuthService(process.env.REACT_APP_AUTH0_CLIENT_ID, process.env.REACT_APP_AUTH0_DOMAIN);
 
-const AppContainer = () =>
-  <App auth={auth} />;
+axios.post('http://localhost:3001/api/reviews', {}, { headers: { Authorization: `Bearer ${authService.getAccessToken()}` } }, response => {
+  console.log(response);
+});
 
-export default AppContainer;
+class AppContainer extends Component {
+  render () {
+    const { auth } = this.props;
+
+    return <App auth={auth} />;
+  }
+}
+
+AppContainer.propTypes = {
+  auth: PropTypes.shape({})
+};
+
+const mapStateToProps = state =>
+  ({
+    auth: authService
+  });
+
+export default connect(mapStateToProps)(AppContainer);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+import 'babel-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import Routes from './routes';
+import configureStore from './reducers';
 
 import './reset.css';
 
-import injectTapEventPlugin from 'react-tap-event-plugin';
+const store = configureStore();
 
 injectTapEventPlugin();
 
 ReactDOM.render(
-  <Routes />,
+  <Provider store={store}>
+    <Routes store={store} />
+  </Provider>,
   document.getElementById('root')
 );
