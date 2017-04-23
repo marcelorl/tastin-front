2d880418026d887f4cfc4cc1e1cf2a36f403e039
marcelorl <marcelo_rl2@hotmail.com>
2017-04-23 00:55:31 -0300

    small fix

--- src/components/molecules/MapMarker/MapMarker.js
+++ src/components/molecules/MapMarker/MapMarker.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Place from 'material-ui/svg-icons/maps/place';
 
@@ -6,15 +6,13 @@ import './MapMarker.css';
 
 const pointerColor = '#89849b';
 
-const Marker = (props) => {
-  console.log('MARKER', props);
-  return(<div className='marker'>
+const Marker = () =>
+  <div className='marker'>
     <div className='marker__pin'>
       <Place className="marker__pin__pointer" color={pointerColor} />
       <div className='marker__pin__pulse' />
     </div>
-  </div>);
-}
+  </div>;
 
 Marker.PropTypes = {
   hover: PropTypes.bool,
